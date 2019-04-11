a06ca2ced1237d9da6a5dc9fbe7d4a75ee3fd1af
valtteri <valtteri.koivunen@hotmail.com>
2019-04-11 10:24:06 +0100

    language changes

---
+++ src/main/java/Project/Investment/webcontroller/LanguageController.java
@@ -0,0 +1,21 @@
+package Project.Investment.webcontroller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class LanguageController {
+	
+	//Englanniksi
+	@RequestMapping(value="/commentlist?language=en", method=RequestMethod.GET)
+	public String English() {
+	return "redirect:commentlist";	
+	}
+	
+	@RequestMapping(value="/commentlist?language=fr", method=RequestMethod.GET)
+	public String French() {
+	return "redirect:commentlist";
+	}
+
+}
